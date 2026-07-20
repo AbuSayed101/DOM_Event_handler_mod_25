0b2b0f36d8036a544240e8ffd9d3f74bb35d18a8
sayeed <abusayeedkhudri55880@gmail.com>
2026-07-21 01:03:28 +0600

    eventlistener is too hard

--- js/handler.js
+++ js/handler.js
@@ -9,5 +9,23 @@ document.getElementById('btn-update-title').addEventListener('click',function(){
       document.getElementById('btn-login').addEventListener('click',function(){
         //console.log('login button clicked')
         const userInfoEl=document.getElementById('user-info');
-        userInfoEl.innerText='user logged in successfull'
+        userInfoEl.innerText='login successfully';
       })
+
+
+//    set event listener
+
+    document.getElementById('btn-name').addEventListener('click',function(){
+        console.log('click the name button');
+        // get the text from the input field
+
+       const inputName=document.getElementById('name-input');
+       console.log('inputName');
+       const name1=inputName.value;
+       console.log('name',name1);
+
+       //set the name
+       const namP=document.getElementById('');
+       namP.innerText=name;
+    })
+     
