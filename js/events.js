7c32a437fbeecfc3fa0b517c9a0f3491bb727201
sayeed <abusayeedkhudri55880@gmail.com>
2026-07-20 20:02:54 +0600

    using addEventListener to update this project

---
+++ js/events.js
@@ -0,0 +1,33 @@
+//  option 2 to handle 
+ 
+ function makeYellow() {
+        document.body.style.backgroundColor = 'yellow'
+    }
+    function makeRed() {
+        document.body.style.backgroundColor = 'red'
+    }
+    function sayeed() {
+        document.body.style.backgroundColor = 'blue'
+    }
+
+
+     const btnMakeBlue = document.getElementById('btn-make-blue');
+    // console.log(btnMakeBlue);
+   btnMakeBlue.onclick=function makeBlue(){
+    document.body.style.backgroundColor='blue';
+   }
+   const wife = document.getElementById('bou');
+//    console.log(wife);
+wife.onclick=function makeorange(){
+    document.body.style.backgroundColor='orange';
+}
+const examini = document.getElementById('candidate');
+examini.onclick=function dhaka(){
+    document.body.style.backgroundColor='skyblue';}
+
+
+    const btnMakePurple=document.getElementById('btn-make-purple');
+   btnMakePurple.onclick=makePurle;
+   function makePurle(){
+    document.body.style.backgroundColor='purple';
+   }
